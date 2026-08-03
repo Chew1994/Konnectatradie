7c7e348afa494a2f4d41a01658f0512e33faae14
George McGlone <georgemcglone94@gmail.com>
2026-08-03 13:00:06 +0100

    Split vendor bundles for faster loading

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+﻿import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: "react-vendor",
+              test: /node_modules[\\/]react(?:-dom)?[\\/]/
+            },
+            {
+              name: "supabase-vendor",
+              test: /node_modules[\\/]@supabase[\\/]/
+            },
+            {
+              name: "icons-vendor",
+              test: /node_modules[\\/]lucide-react[\\/]/
+            }
+          ]
+        }
+      }
+    }
+  }
+});
